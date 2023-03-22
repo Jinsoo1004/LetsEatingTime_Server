a148ce3553596c5529aad26ead14c43cb0e16057
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-03-22 11:43:15 +0900

    init

---
+++ src/main/java/com/example/letseatingtime_server/LetsEatingTimeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.letseatingtime_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsEatingTimeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsEatingTimeServerApplication.class, args);
+	}
+
+}
