a5d827b7439844c4c60fc3a27a32b3718ae2c113
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2023-06-28 19:56:16 +0900

    숨길것만

--- src/main/java/com/example/let/LetApplication.java
+++ src/main/java/com/example/let/LetApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:/database.properties")
 public class LetApplication {
 
 	public static void main(String[] args) {
