27be5def0d3122b58618248eef6bad5fe4f84e25
이찬혁 <109561938+dogbokchif@users.noreply.github.com>
2023-08-11 16:14:28 +0900

    승인 안된 유저 가져오지 않기

--- src/main/java/com/example/let/service/impl/FileUploadServiceImpl.java
+++ src/main/java/com/example/let/service/impl/FileUploadServiceImpl.java
@@ -137,6 +137,7 @@ public class FileUploadServiceImpl implements FileUploadService {
         while (! row.getCell(0).getStringCellValue().equals("EndPoint")) {
             User user = userMapper.getById(row.getCell(2).getStringCellValue());
             accessMapper.delete(user.getId());
+            System.out.println(user.getId());
             if (row.getCell(4).getStringCellValue().equals("O")) {
                 accessService.register(user.getId(), grantor, "breakfast");
                 accessService.register(user.getId(), grantor, "lunch");
